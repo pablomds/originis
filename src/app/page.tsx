bfbc376f0f4b6d33137adccff3e626018b40d472
pablomds <moraisdesouzapablo@gmail.com>
2025-04-04 23:33:19 +0200

    using font inter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import NavbarPublic from "@/components/Header/NavbarPublic";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
+      <NavbarPublic/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -98,6 +100,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </>
   );
 }
---
+++ src/components/Header/NavbarPublic.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+
+const NavbarPublic = () => {
+  return (
+    <nav className="px-[25px] py-[22px] md:pl-[96px] md:pr-[27px] flex flex-row justify-between items-center h-[77px]">
+      <div className="flex flex-row items-center gap-x-[11px]">
+        <img src="/logo.svg" alt="Originis Logo" className="h-[11px] w-auto" />
+        <p className="text-[18px] text-indigo-600">Originis</p>
+      </div>
+      <div className="flex gap-x-[17px] items-center">
+        <Link
+          className="text-slate-600 hover:text-slate-800 text-[15px] font-semibold"
+          href={"/login"}
+        >
+          Login
+        </Link>
+        <Link
+          className="bg-indigo-600 hover:bg-indigo-800 text-[15px] py-2 px-3 rounded-[4px] h-[41px] font-semibold text-white"
+          href={"/signup"}
+        >
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarPublic
