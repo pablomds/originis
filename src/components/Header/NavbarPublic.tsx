103d86fb410579088b907793a01b3c490d0a2904
pablomds <moraisdesouzapablo@gmail.com>
2025-04-12 16:15:30 +0200

    adapting design (hover, text size, etc)

--- src/components/Header/NavbarPublic.tsx
+++ src/components/Header/NavbarPublic.tsx
@@ -4,20 +4,20 @@ import React from 'react'
 
 const NavbarPublic = () => {
   return (
-    <nav className="px-[25px] py-[22px] md:pl-[96px] md:pr-[27px] border-b border-[#E5E7EB] flex flex-row justify-between items-center h-[77px]">
+    <nav className="px-[25px] py-[22px] md:pl-[96px] md:pr-[27px] border-b-2 border-[#E5E7EB] flex flex-row justify-between items-center h-[77px]">
       <div className="flex flex-row items-center gap-x-[11px]">
-        <img src="/logo.svg" alt="Originis Logo" className="h-[11px] w-auto" />
-        <p className="text-[18px] text-indigo-600">Originis</p>
+        <img src="/logo.svg" alt="Originis Logo" className="h-[11px] w-auto md:h-[37px] md:w-[30px]" />
+        <p className="text-[18px] md:text-[24px] text-indigo-600">Oríginis</p>
       </div>
       <div className="flex gap-x-[17px] items-center">
         <Link
-          className="text-slate-600 hover:text-slate-800 text-[15px] font-semibold"
+          className="text-[#6369E9] hover:text-[#4147C7] text-[15px] font-semibold"
           href={"/login"}
         >
           Login
         </Link>
         <Link
-          className="bg-indigo-600 hover:bg-indigo-800 text-[15px] py-2 px-3 rounded-[4px] h-[41px] font-semibold text-white"
+          className="bg-indigo-600 hover:bg-indigo-700 text-[15px] py-2 px-3 rounded-[4px] h-[41px] font-semibold text-white"
           href={"/signup"}
         >
           Sign Up
