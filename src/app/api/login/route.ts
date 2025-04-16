46ba045efe841c6b4efd07f7ac24aa9dad7c235b
pablomds <moraisdesouzapablo@gmail.com>
2025-04-16 18:43:14 +0200

    apis routes

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,25 @@
+'use server'
+import { auth } from '@/utils/auth/firebase-admin';
+import { cookies } from 'next/headers';
+
+export async function POST(req: Request) {
+  const { idToken } = await req.json();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
+
+  try {
+    const authInstance = await auth;
+    const sessionCookie = await authInstance.createSessionCookie(idToken, { expiresIn });
+    const cookiesStore = await cookies();
+    cookiesStore.set('session', sessionCookie, {
+      maxAge: expiresIn / 1000,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+    });
+
+    return new Response(JSON.stringify({ status: 'success' }), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: 'Failed to create session' }), { status: 401 });
+  }
+}
